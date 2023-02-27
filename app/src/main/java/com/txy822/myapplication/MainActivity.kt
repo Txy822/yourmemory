d60260a09c8eb88e3ea1f217d1a9789cb7558068
tesfahun <y.w.tesfahun@gmail.com>
2023-02-27 17:23:59 +0000

    Recycler view and text view added

--- app/src/main/java/com/txy822/myapplication/MainActivity.kt
+++ app/src/main/java/com/txy822/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.txy822.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.txy822.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumPairs: TextView
+    private lateinit var tvNumMoves: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+       // setContentView(binding.root)
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves =findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
